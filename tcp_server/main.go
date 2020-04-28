7811f4c1ee57895caaf51619b5ca8e124ffad316
Pavel Ivanov <pablospaniard@users.noreply.github.com>
2020-04-28 15:34:03 +0200

    initial commit

---
+++ tcp_server/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+
+		io.WriteString(conn, "\nHello from TCP server\n")
+
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	err := conn.SetDeadline(time.Now().Add(10 * time.Second))
+	if err != nil {
+		log.Println("CONNECTION TIMEOUT FAILED")
+	}
+
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		fmt.Fprintf(conn, "I heard you say: %s\n", ln)
+	}
+	defer conn.Close()
+	fmt.Println("***CONNECTION CLOSED***")
+}
